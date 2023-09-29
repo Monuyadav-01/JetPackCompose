35c333a886e82bf4e8670393141d108fb669be3e
Monu <105378383+PROGRAMMINGKING8055@users.noreply.github.com>
2023-09-29 22:00:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+//            Column{
+//                Text(text = "Monu", color = Color.Blue  )
+//                Text(text = "Yadav" , color= Color.Red)
+//                Row {
+//                    Text(text = "Monu Yadav is Good boy")
+//                    Text(text = "this is good")
+//                }
+//            }
+//            Text(
+//                text = "Monu",
+//                color = Color.Red,
+//                fontFamily = FontFamily.Monospace,
+//                modifier = Modifier
+//                    .background(
+//                        Color.Blue,
+//                    )
+//                    .fillMaxWidth(1f)
+//                    .fillMaxHeight(.1f),
+//                textAlign = TextAlign.Center
+//            )
+//
+//            val array = arrayOf("Monu","Monika", "Nainika","Daksh")
+//            Column {
+//                for ( name in array){
+//                    Text(text = name)
+//                }
+//            }
+
+
+        }
+    }
+
+
+}
+
+
